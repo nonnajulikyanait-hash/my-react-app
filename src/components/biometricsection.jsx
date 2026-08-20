ab88c77ba674ef7a7a2a46c4e4e71ae53fd99834
Նոննա <nonna.julikyan.ait@gmail.com>
2026-08-20 23:03:04 +0400

    gd

--- src/components/biometricsection.jsx
+++ src/components/biometricsection.jsx
@@ -29,15 +29,15 @@ function BiometricSection() {
     <section className="py-20 lg:py-32 bg-white overflow-hidden mt-6">
       <div className="max-w-7xl mx-auto px-6 lg:px-10 flex flex-col lg:flex-row items-center justify-between">
         
-        {/* Ձախ մաս (Ճիշտ ձևավորված եռանկյուն և դեմքի նկար) */}
+        {/* Ձախ մաս (Իրականին լիովին համապատասխանող մեծ ու լայն եռանկյուն և դեմք) */}
         <div className="lg:w-1/2 flex justify-center items-center relative mb-12 lg:mb-0">
-          <div className="relative flex justify-center items-center w-[440px] h-[440px]">
+          <div className="relative flex justify-center items-center w-[460px] h-[460px]">
             
-            {/* Հետնամասի մանուշակագույն ճշգրիտ եռանկյունի (Evoca ոճով՝ լայն և կլորացված) */}
+            {/* Հետնամասի լայն և ճշգրիտ կլորացված եռանկյունի */}
             <div 
-              className="absolute w-[400px] h-[340px] bg-[#6400DC] pointer-events-none"
+              className="absolute w-[420px] h-[380px] bg-[#6400DC] pointer-events-none"
               style={{
-                borderRadius: "48px",
+                borderRadius: "56px",
                 clipPath: "polygon(50% 100%, 0% 0%, 100% 0%)"
               }}
             ></div>
@@ -46,7 +46,7 @@ function BiometricSection() {
             <img 
               src={faces[faceIndex]} 
               alt="Biometric Identification" 
-              className={`w-full max-w-[390px] h-auto object-contain relative z-10 transition-opacity duration-500 ${
+              className={`w-full max-w-[410px] h-auto object-contain relative z-10 transition-opacity duration-500 ${
                 isVisible ? 'opacity-100 scale-100' : 'opacity-0 scale-95'
               }`}
             />
