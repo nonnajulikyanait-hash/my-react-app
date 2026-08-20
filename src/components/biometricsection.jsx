be7c8b8908abfd46ff5e7fd2c8bdfbe53f326d76
Նոննա <nonna.julikyan.ait@gmail.com>
2026-08-20 23:58:27 +0400

    ghg

--- src/components/biometricsection.jsx
+++ src/components/biometricsection.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import BiometricTriangle from './BiometricTriangle';
+import BiometricTriangle from './biometrictriangle';
 
 // Բոլոր 3 դեմքերի նկարների զանգվածը
 const faces = [
