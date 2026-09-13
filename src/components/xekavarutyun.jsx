5ed58425c671d1aa04de341a52baccdeeda6d2f6
Նոննա <nonna.julikyan.ait@gmail.com>
2026-09-13 23:11:13 +0400

    vbhdc

---
+++ src/components/xekavarutyun.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Xekavarutyun() {
+  return (
+    <div>
+      {/* Բովանդակություն */}
+    </div>
+  );
+}
+
+export default Xekavarutyun; // <--- Սա պարտադիր է
