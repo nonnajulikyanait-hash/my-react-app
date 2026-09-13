73054c3ebac8471bb164df51436231f1c833691e
Նոննա <nonna.julikyan.ait@gmail.com>
2026-09-13 23:05:03 +0400

    Add Online component and fix imports

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,8 @@ import AkntartayinVcharumner from './components/akntartayinvcharumner';
 import Mermasin from './components/mermasin';
 import Karucvacq from './components/karucvacq';
 import Bajneterer from './components/bajneterer';
-import Xekavarutyun from './components/xekavarutyun'; // Ավելացվել է Ղեկավարության ներմուծումը
-import Online from './online'; // Ավելացվել է Online բաղադրիչի ներմուծումը
+import Xekavarutyun from './components/xekavarutyun'; 
+import Online from './online'; 
 
 function App() {
   const [activeTab, setActiveTab] = useState('home');
