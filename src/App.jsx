25953ffc2b1d30cec9cddaeeca4b533586995a20
Նոննա <nonna.julikyan.ait@gmail.com>
2026-09-13 23:09:07 +0400

    dgvsb

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import Mermasin from './components/mermasin';
 import Karucvacq from './components/karucvacq';
 import Bajneterer from './components/bajneterer';
 import Xekavarutyun from './components/xekavarutyun'; 
-import Online from './Online';
+import Online from './online';
 
 function App() {
   const [activeTab, setActiveTab] = useState('home');
